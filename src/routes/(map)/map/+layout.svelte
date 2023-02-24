a34326e8b5651072afe4540703dbc5e9e1cab2fb
Larry Eitel <larry@eitel.com>
2023-02-24 11:00:36 -0600

    add home button to map

--- src/routes/(map)/map/+layout.svelte
+++ src/routes/(map)/map/+layout.svelte
@@ -1,1 +1,19 @@
 <slot />
+
+<a href="/map">
+	<button class="btn variant-filled-primary cursor-pointer">Map</button>
+</a>
+
+<div class="fixed left-0 top-0">
+    <div class="relative p-3" >
+        <a href="/">
+            <button class="btn variant-filled-primary cursor-pointer font-bold">Home</button>
+        </a>
+    </div>
+</div>
+
+<style>
+	.fixed {
+		position: fixed;
+	}
+</style>
